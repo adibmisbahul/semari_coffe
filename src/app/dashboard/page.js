c04adb41a3e833d411ac11c9916e17dbe20ded71
adibmisbahul <adibmisbahul101812@gmail.com>
2025-07-20 15:16:05 +0700

    add card

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,32 +1,28 @@
 "use client";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
-import { useEffect, useState } from "react";
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  themeMaterial,
+} from "ag-grid-community";
+import { useEffect, useState, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react";
 import supabase from "@/lib/supabaseClient";
-import { createRoot } from "react-dom/client";
-import { AgCharts } from "ag-charts-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const Dashboard = () => {
-  const [options, setOptions] = useState({
-    data: [
-      { month: "Jan", avgTemp: 2.3, iceCreamSales: 162000 },
-      { month: "Mar", avgTemp: 6.3, iceCreamSales: 302000 },
-      { month: "May", avgTemp: 16.2, iceCreamSales: 800000 },
-      { month: "Jul", avgTemp: 22.8, iceCreamSales: 1254000 },
-      { month: "Sep", avgTemp: 14.5, iceCreamSales: 950000 },
-      { month: "Nov", avgTemp: 8.9, iceCreamSales: 200000 },
-    ],
-    // Series: Defines which chart type and data to use
-    series: [{ type: "bar", xKey: "month", yKey: "iceCreamSales" }],
-  });
+  const [orderToday, setOrderToday] = useState();
+  const [uangMasuk, setUangMasuk] = useState();
+  const gridStyle = useMemo(() => ({ height: "70%", width: "90%" }), []);
 
   useEffect(() => {
     const fetchData = async () => {
       const { data, error } = await supabase.from("transaction").select();
       if (error) alert(error);
       else setRowData(data);
+      setOrderToday(data.length);
       console.log(data);
     };
     fetchData();
@@ -42,20 +38,27 @@ const Dashboard = () => {
     { field: "order_date" },
   ]);
 
+  const total = rowData.reduce((acc, item) => acc + item.price, 0);
+  console.log(total);
+
   return (
     <div className="w-[100%] h-[100svh] flex flex-col items-center bg-slate-950 gap-2">
-      <div className="flex flex-wrap">
-        <div></div>
-      </div>
-      <div className="w-[95%] h-[40svh] border rounded">
-        <AgCharts options={options} style={{ width: 370, height: 300 }} />
+      <div className="flex flex-wrap text-white w-full p-2 gap-2 justify-center">
+        <div className="border rounded w-[48%] h-24 border-zinc-600 p-1">
+          <h1>order today</h1>
+          <h1>{orderToday}</h1>
+        </div>
+        <div className="border rounded  w-[48%] h-24 border-zinc-600 p-1 ">
+          <h1>Uang masuk</h1>
+          <h1>{total}</h1>
+        </div>
       </div>
 
-      <div className="w-[95%] h-[40svh] border rounded">
+      <div style={gridStyle} className="the">
         <AgGridReact
           rowData={rowData}
           columnDefs={colDefs}
-          style={{ height: 400, width: 300 }}
+          theme={themeMaterial}
         />
       </div>
     </div>
@@ -63,13 +66,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-{
-  /* <div style={{ height: 500, width: 400 }} className="bg-blue-600">
-<AgGridReact
-  rowData={rowData}
-  columnDefs={colDefs}
-  style={{ height: 500, width: 400 }}
-/>
-</div> */
-}
